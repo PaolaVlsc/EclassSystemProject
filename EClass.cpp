174064d7da32f756e58dbe117026609910d50cce
urmrs98 <paolavlsc170698@gmail.com>
2022-07-07 22:22:57 +0300

    Eclass class

---
+++ EClass.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Velasco on 7/7/2022.
+//
+
+#include "EClass.h"
---
+++ EClass.h
@@ -0,0 +1,14 @@
+//
+// Created by Velasco on 7/7/2022.
+//
+
+#ifndef ECLASSSYSTEMPROJECT_ECLASS_H
+#define ECLASSSYSTEMPROJECT_ECLASS_H
+
+
+class EClass {
+
+};
+
+
+#endif //ECLASSSYSTEMPROJECT_ECLASS_H
